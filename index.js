cb8827d54f40ea5b001b347e8bb3dabdb5a2c8fc
Chocolate House Records <chocolatehouserecords@Chocolates-Air.lan>
2025-07-21 21:00:41 -0700

    Use new headless mode for Puppeteer

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const fs = require("fs");
     console.log("🚀 Launching Puppeteer with stealth...");
 
     const browser = await puppeteerExtra.launch({
-      headless: true,
+      headless: new,
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
